1d015b2ca25efa861f71b5531392e878b1864db4
miku-o <2438175594@qq.com>
2022-10-21 14:15:38 +0800

    🍵

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+#app {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+html,
+body {
+  min-height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+@media screen and (max-width: 900px) {
+  html {
+    font-size: 14px;
+  }
+}
+
+a:link{color: #939393;}  /*超链接默认样式*/
+a:visited{color: #939393;}  /*超链接被访问后的样式*/
+a:hover{color: green;}   /*鼠标经过超链接的样式*/
+a:active{color: black;}  /*超链接被激活时的样式*/
+a{text-decoration: none;}  /*完全清除超链接下划线效果*/
+img{vertical-align:middle;}
+</style>
+
+
+
